297e2901b7d0dc8fb7a43ec3913f1138cf37be00
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-14 08:00:33 +0530

    Register income database migration

--- ExpenseTracker/app/src/main/java/com/expensetracker/app/di/AppModules.kt
+++ ExpenseTracker/app/src/main/java/com/expensetracker/app/di/AppModules.kt
@@ -30,6 +30,7 @@ val databaseModule = module {
                 ExpenseDatabase.MIGRATION_2_3,
                 ExpenseDatabase.MIGRATION_3_4,
                 ExpenseDatabase.MIGRATION_4_5,
+                ExpenseDatabase.MIGRATION_5_6,
             )
             .fallbackToDestructiveMigrationOnDowngrade()
             .build()
