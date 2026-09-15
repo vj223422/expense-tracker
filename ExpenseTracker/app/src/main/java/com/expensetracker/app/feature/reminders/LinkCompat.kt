442c75edf36393ab21205a8a0965cc839b3719ae
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-15 20:56:21 +0530

    Remove obsolete reminder icon compatibility file

--- ExpenseTracker/app/src/main/java/com/expensetracker/app/feature/reminders/LinkCompat.kt
+++
@@ -1,8 +0,0 @@
-package com.expensetracker.app.feature.reminders
-
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Event
-import androidx.compose.ui.graphics.vector.ImageVector
-
-private val reminderLinkIcon: ImageVector
-    get() = Icons.Default.Event
