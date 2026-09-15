17dde8b468ecab70f4089444fc5413156ae95a65
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-15 20:34:28 +0530

    Provide compatible Link icon

---
+++ ExpenseTracker/app/src/main/java/com/expensetracker/app/feature/reminders/LinkIconExtension.kt
@@ -0,0 +1,7 @@
+package androidx.compose.material.icons.filled
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val Icons.Filled.Link: ImageVector
+    get() = Icons.Filled.EventRepeat
