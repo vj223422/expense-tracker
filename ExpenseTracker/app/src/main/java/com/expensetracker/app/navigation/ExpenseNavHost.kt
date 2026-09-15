91383b6a2a8620f95590ee10d99a5c09ae8b99ab
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-15 20:04:35 +0530

    Fix missing slideOutVertically import

--- ExpenseTracker/app/src/main/java/com/expensetracker/app/navigation/ExpenseNavHost.kt
+++ ExpenseTracker/app/src/main/java/com/expensetracker/app/navigation/ExpenseNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
