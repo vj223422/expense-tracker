b7912659b6a91d221f889aa96cf1f4cd493075cf
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-15 20:34:12 +0530

    Fix reminder icon compilation

---
+++ ExpenseTracker/app/src/main/java/com/expensetracker/app/feature/reminders/LinkCompat.kt
@@ -0,0 +1,8 @@
+package com.expensetracker.app.feature.reminders
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
+
+private val reminderLinkIcon: ImageVector
+    get() = Icons.Default.EventRepeat
+
