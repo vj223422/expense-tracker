c82812b35e07d1bbc1ed8f3e53580bdc28c5a26f
vj223422 <99543233+vj223422@users.noreply.github.com>
2026-09-16 15:10:18 +0530

    Register salary-cycle database migration

--- ExpenseTracker/app/src/main/java/com/expensetracker/app/di/AppModules.kt
+++ ExpenseTracker/app/src/main/java/com/expensetracker/app/di/AppModules.kt
@@ -25,7 +25,7 @@ import org.koin.dsl.module
 val databaseModule = module {
     single {
         Room.databaseBuilder(androidContext(), ExpenseDatabase::class.java, ExpenseDatabase.DATABASE_NAME)
-            .addMigrations(ExpenseDatabase.MIGRATION_1_2, ExpenseDatabase.MIGRATION_2_3, ExpenseDatabase.MIGRATION_3_4, ExpenseDatabase.MIGRATION_4_5, ExpenseDatabase.MIGRATION_5_6, ExpenseDatabase.MIGRATION_6_7)
+            .addMigrations(ExpenseDatabase.MIGRATION_1_2, ExpenseDatabase.MIGRATION_2_3, ExpenseDatabase.MIGRATION_3_4, ExpenseDatabase.MIGRATION_4_5, ExpenseDatabase.MIGRATION_5_6, ExpenseDatabase.MIGRATION_6_7, ExpenseDatabase.MIGRATION_7_8)
             .fallbackToDestructiveMigrationOnDowngrade().build()
     }
     single { get<ExpenseDatabase>().expenseDao() }
